9604f0cdd86effd9eb87c07ab90eff28995b0db7
Manojkumar886 <maddymano434@gmail.com>
2023-03-10 07:54:39 +0530

    HTML CSS JS is OVER and BOOTSTRAP is Started

---
+++ JavaScript/BasicFUnctions.js
@@ -0,0 +1,19 @@
+function basic1()
+{
+    alert("A js functions is a block of code designed to perform a particukar task.(function and fat arrow function)")
+}
+
+function Parameterizedfunction(myname,age)
+{
+    document.write("my name is "+myname+" my age is :"+age)
+}
+
+let FatArrowFunction=()=>
+{
+    console.log("this is fat arrow function.Arrow functions allow us to use the fat arrow => operator to quickly define JavaScript functions, with or without parameters.")
+}
+
+const fatwithparam=(a,b)=>
+{
+    return a*b*b+"this value is multiple by "+a+"*"+b+"*"+b;
+}
---
+++ JavaScript/IRCTCFORM.js
@@ -0,0 +1,54 @@
+let Action=()=>
+{
+    let a=document.forms['irctc'].fno.value;
+    let b=document.forms['irctc'].Svalue.value;
+    let c=document.forms['irctc'].pin.value;
+    let d=document.forms['irctc'].email1.value;
+    let e=document.forms['irctc'].pass.value;
+
+    let active=true;
+    if(e.length<=0)
+    {
+        document.getElementById('passing').innerHTML="Password should't empty";
+        active=false;
+    }
+    else if(!((/^[a-zA-Z0-9!@#$%^&*]{6,16}$/).test(e)))
+    {
+        document.getElementById('passing').innerHTML="Password using special charcter and min6 max16 limits";
+        active=false;   
+    }
+
+    if(d.length<=0)
+    {
+        document.getElementById('edetails').innerHTML="Email id should't empty";
+        active=false;
+    }
+    else if(!((/^(?=.*[0-9])(?=.+[@.])[a-z0-9@.]+$/).test(d)))
+    {
+        document.getElementById('edetails').innerHTML="Email id used by @ . symbol";
+        active=false;
+    }
+    if(a.length<=0)
+    {
+        document.getElementById('fdetails').innerHTML="Flat input should't empty";
+        active=false;
+    }
+    else if(!((/^[#.0-9a-zA-Z\s,-/]+$/).test(a)))
+    {
+        document.getElementById('fdetails').innerHTML="first flat no/ address/doorno";
+        active=false;
+    }
+
+    if(b.length<=0)
+    {
+        document.getElementById('sdetails').innerHTML="Street name should't empty";
+        active=false;   
+    }
+    else if(!(/^[A-Za-z\s]+$/).test(b))
+    {
+        document.getElementById('sdetails').innerHTML="Street name should't use numerical numbers";
+        active=false; 
+    }
+    return active;
+
+}
